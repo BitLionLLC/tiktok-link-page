c7aebbec3239c6832f7f155492646c9bade35725
Grant Reighard <grant.reighard@gmail.com>
2021-10-23 09:43:14 -0400

    Updates Twitch API to move off deprecated call.

--- src/App.js
+++ src/App.js
@@ -11,22 +11,32 @@ function App() {
   const [isLiveOnTwitch, setIsLiveOnTwitch] = useState(false);
 
   useEffect(() => {
-    const config = {
-      headers: {
-        'Client-ID': 'fnqmo60hbl4l677epnry648yxie2tn',
-        'Accept': 'application/vnd.twitchtv.v5+json'
-      }
-    }
-
     axios
-      .get('https://api.twitch.tv/kraken/streams/433762691', config)
+      .post("https://id.twitch.tv/oauth2/token?client_id=fnqmo60hbl4l677epnry648yxie2tn&client_secret=8e1ivl0wa63bskpv6a4n20ukpymqvg&grant_type=client_credentials")
       .then(res => {
-        if (res.data.stream.broadcast_platform === "live") {
-          setIsLiveOnTwitch(true);
+        const token = res.data.access_token;
+        const config = {
+          headers: {
+            'Client-ID': 'fnqmo60hbl4l677epnry648yxie2tn',
+            'Accept': 'application/vnd.twitchtv.v5+json',
+            'Authorization': `Bearer ${token}`
+          }
         }
+
+        axios
+          .get('https://api.twitch.tv/helix/search/channels?query=keefler_elf', config)
+          .then(res1 => {
+            const correctChannel = res1.data.data.find(channel => channel.id === "433762691")
+            if (correctChannel.is_live) {
+              setIsLiveOnTwitch(true);
+            }
+          })
+          .catch(err => {
+            console.error(err);
+          })
       })
       .catch(err => {
-        console.error(err);
+        console.error(err)
       })
   }, [])
 
