0ab4e2c13eab8f7dcc9060ac9d17b70987d827e4
Grant Reighard <grant.reighard@gmail.com>
2023-04-07 21:04:25 -0400

    Adds TikTok, renames and rearranges other links.

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -2,16 +2,17 @@ import React from 'react';
 import SingleLink from './singleLink';
 
 const links = [
-  {link: "https://open.spotify.com/artist/3t3zKVdiWERcdDHg6ei89c", title: "Music (Spotify)", icon: ["fab", "spotify"], color: "#1DB954"},
-  {link: "https://open.spotify.com/artist/2RepJuGMScwrv8zoAufnmf", title: "More music (Spotify)", icon: ["fab", "spotify"], color: "#1DB954"},
-  {link: "https://music.apple.com/us/artist/keefler/1567547480", title: "Music (Apple Music)", icon: ["fab", "apple"], color: "#fc3c44"},
-  {link: "https://music.apple.com/us/artist/brayden-bauer/1476482299", title: "More music (Apple Music)", icon: ["fab", "apple"], color: "#fc3c44"},
+  {link: "https://open.spotify.com/artist/3t3zKVdiWERcdDHg6ei89c", title: "Keefler on Spotify", icon: ["fab", "spotify"], color: "#1DB954"},
+  {link: "https://music.apple.com/us/artist/keefler/1567547480", title: "Keefler on Apple Music", icon: ["fab", "apple"], color: "#fc3c44"},
+  {link: "https://open.spotify.com/artist/2RepJuGMScwrv8zoAufnmf", title: "Brayden Bauer on Spotify", icon: ["fab", "spotify"], color: "#1DB954"},
+  {link: "https://music.apple.com/us/artist/brayden-bauer/1476482299", title: "Brayden Bauer on Apple Music", icon: ["fab", "apple"], color: "#fc3c44"},
   {link: "https://www.twitch.tv/keefler_elf", title: "Twitch", icon: ["fab", "twitch"], color: "#6441a5"},
   {link: "https://discord.gg/MsEwjY2", title: "Join the Discord!", icon: ["fab", "discord"], color: "#5865F2"},
   {link: "https://twitter.com/keefler_elf", title: "Twitter", icon: ["fab", "twitter"], color:"#00acee"},
-  {link: "https://www.instagram.com/keefler_elf/", title: "Instagram", icon: ["fab", "instagram"], color: "#3f729b"},
-  {link: "https://www.bonfire.com/store/braydens-very-diverse-store/", title: "Merch", icon: ["fas", "shopping-cart"]},
-  {link: "https://www.yasglass.com/not-high-pipes.html", title: "Pipes", icon: ["fas", "joint"]},
+  {link: "https://www.instagram.com/keefler_elf", title: "Instagram", icon: ["fab", "instagram"], color: "#3f729b"},
+  {link: "https://www.tiktok.com/@keefler_elf", title: "TikTok", icon: ["fab", "tiktok"]},
+  {link: "https://www.bonfire.com/store/braydens-very-diverse-store/", title: "'Not High' Merch", icon: ["fas", "shopping-cart"]},
+  {link: "https://www.yasglass.com/not-high-pipes.html", title: "'Not High' Pipes", icon: ["fas", "joint"]},
   {link: "https://onlyfans.com/keefler_elf", title: "OnlyFans", icon: ["fas", "lock"], color: "#00AFF0"},
   {link: "https://www.amazon.com/hz/wishlist/ls/2NDKMZL4ULQ61", title: "Amazon Wishlist", icon: ["fab", "amazon"], color: "#b16501"},
 ]
